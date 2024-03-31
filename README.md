d9c8ae892091632af434d42127ae6f5843ad0fff
sxh705 <2362989228@qq.com>
2024-03-31 17:41:45 +0800

    1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 README.md是每个目录的首页
 
 # sxh的个人博客
+
+<button onClick="localStorage.setItem('xh_password', '');location.reload();">退出登录</button>
--- src/globalSetting.js
+++ src/globalSetting.js
@@ -2,9 +2,12 @@
 const globalSetting = {
     // 博客名称
     xh_name: 'sxh705的个人博客',
-
+    
     // 博客子文件夹列表
     // 修改此信息时 应同步修改各级文件夹下的 _sidebar.md
     xh_folder: ['老夏', '日记', '企业', '学校'],
+
+    // 首次登录需要输入密码
+    xh_password: '7051',
 }
 
